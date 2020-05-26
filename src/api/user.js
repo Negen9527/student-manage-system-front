894ec4e82c7ac533719d39a9f1484358b8e17560
Negen <1096041251@qq.com>
2020-05-26 12:27:01 +0800

    新增注册页面

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
+export function register(data) {
+  return request({
+    url: '/user/create',
+    method: 'post',
+    data
+  })
+}
+
 export function login(data) {
   return request({
     url: '/login?username='+ data.username +'&password=' + data.password,
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left" v-show="loginFormVisble">
       <div class="title-container">
         <h3 class="title">Login</h3>
       </div>
@@ -41,19 +40,94 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
 
       <!-- <div class="tips">
         <span style="margin-right:20px;">username: admin</span>
         <span> password: any</span>
       </div> -->
-
     </el-form>
+
+
+    <el-form class="login-form" ref="registerForm" :model="registerForm" :rules="registerRules" label-position="left" v-show="registerFormVisble">
+      <div class="title-container">
+        <h3 class="title">Register</h3>
+      </div>
+
+      <el-form-item prop="username">
+        <span class="svg-container">
+          <svg-icon icon-class="user" />
+        </span>
+        <el-input
+          ref="username"
+          v-model="registerForm.username"
+          placeholder="Username"
+          name="username"
+          type="text"
+          tabindex="1"
+          auto-complete="on"
+        />
+      </el-form-item>
+
+      <el-form-item prop="password1">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :key="passwordType"
+          ref="password1"
+          v-model="registerForm.password1"
+          :type="passwordType"
+          placeholder="Password"
+          name="password1"
+          tabindex="2"
+          auto-complete="on"
+        />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+
+      <el-form-item prop="password2">
+        <span class="svg-container">
+          <svg-icon icon-class="password" />
+        </span>
+        <el-input
+          :key="passwordType"
+          ref="password2"
+          v-model="registerForm.password2"
+          :type="passwordType"
+          placeholder="Password Again"
+          name="password2"
+          tabindex="2"
+          auto-complete="on"
+        />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+        </span>
+      </el-form-item>
+
+      <!-- <div class="tips">
+        <span style="margin-right:20px;">username: admin</span>
+        <span> password: any</span>
+      </div> -->
+    </el-form>
+
+    <div class="login-form" style="margin-top:0px;padding-top:0px" v-show="loginButtonDiv">
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <el-button :loading="loading" type="warning" style="width:100%;margin-left: 0px;" @click.native.prevent="goRegister">Go Register</el-button>
+    </div>
+
+    <div class="login-form" style="margin-top:0px;padding-top:0px" v-show="registerButtonDiv">
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleRegister">Register</el-button>
+      <el-button :loading="loading" type="warning" style="width:100%;margin-left: 0px;" @click.native.prevent="backLogin">Back Login</el-button>
+    </div>
+
   </div>
 </template>
 
 <script>
 import { validUsername } from '@/utils/validate'
+import { register } from '@/api/user'
 
 export default {
   name: 'Login',
@@ -77,13 +151,27 @@ export default {
         username: '',
         password: ''
       },
+      registerForm: {
+        username: '',
+        password1: '',
+        password2: ''
+      },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
         password: [{ required: true, trigger: 'blur', validator: validatePassword }]
       },
+      registerRules: {
+        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
+        password1: [{ required: true, trigger: 'blur', validator: validatePassword }],
+        password2: [{ required: true, trigger: 'blur', validator: validatePassword }]
+      },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
+      redirect: undefined,
+      loginFormVisble: true,
+      registerFormVisble: false,
+      loginButtonDiv: true,
+      registerButtonDiv: false
     }
   },
   watch: {
@@ -120,7 +208,67 @@ export default {
           return false
         }
       })
+    },
+    handleRegister() {
+      this.$refs.registerForm.validate(valid => {
+        if (valid) {
+          this.loading = true
+          var userName = this.registerForm.username
+          var password = this.registerForm.password1
+          var password2 = this.registerForm.password2
+          if (password != password2) {
+            this.$message({
+              message: "输入密码不一致",
+              type: "error"
+            })
+            this.loading = false
+            return
+          }
+          var data = {
+            userName: userName,
+            password: password
+          }
+          register(data).then(response => {
+            this.$message({
+              message: response.message,
+              type: "warning"
+            })
+            this.loading = false
+            this.reset()
+          }).catch(() => {
+            this.loading = false
+            this.reset()
+          })
+          // this.$store.dispatch('user/login', this.loginForm).then(() => {
+          //   this.$router.push({ path: this.redirect || '/' })
+          //   this.loading = false
+          // }).catch(() => {
+          //   this.loading = false
+          // })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    goRegister(){
+      this.loginFormVisble = false
+      this.registerFormVisble = true
+      this.loginButtonDiv = false
+      this.registerButtonDiv = true
+    },
+    backLogin(){
+      this.loginFormVisble = true
+      this.registerFormVisble = false
+      this.loginButtonDiv = true
+      this.registerButtonDiv = false
+    },
+    reset(){
+      this.registerForm.username = ""
+      this.registerForm.password1 = ""
+      this.registerForm.password2 = ""
     }
+
   }
 }
 </script>
